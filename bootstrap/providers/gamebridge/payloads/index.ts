e16047bc82e4aef8f27916da6a9963d061cf7aca
Tenrys <tenrys.iaido@gmail.com>
2020-10-17 19:18:07 +0200

    order lol

--- bootstrap/providers/gamebridge/payloads/index.ts
+++ bootstrap/providers/gamebridge/payloads/index.ts
@@ -1,10 +1,10 @@
 import AdminNotifyPayload from "./AdminNotifyPayload";
+import BanPayload from "./BanPayload";
 import ChatPayload from "./ChatPayload";
 import ErrorPayload from "./ErrorPayload";
 import JoinLeavePayload from "./JoinLeavePayload";
 import Payload from "./Payload";
 import StatusPayload from "./StatusPayload";
-import BanPayload from "./BanPayload";
 
 export {
 	Payload,
