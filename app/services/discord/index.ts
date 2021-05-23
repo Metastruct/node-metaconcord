c0f78b569b3fe1e3d3e7f3c5289e4c66b098b619
Earu <earu.derg@gmail.com>
2021-05-23 13:51:11 +0200

    gateway is only available after ready event

--- app/services/discord/index.ts
+++ app/services/discord/index.ts
@@ -19,20 +19,6 @@ export class DiscordBot extends Service {
 			this.discord.add(new command(this));
 		}
 
-		const socket = this.discord.client.gateway.socket;
-		const creator = new SlashCreator({
-			applicationID: config.applicationId,
-			token: config.token,
-		});
-
-		creator.withServer(new GatewayServer((handler) => socket.on("INTERACTION_CREATE", handler)));
-		creator
-			// Registers all of your commands in the ./commands/ directory
-			.registerCommandsIn(path.join(__dirname, "commands"))
-			// This will sync commands to Discord, it must be called after commands are loaded.
-			// This also returns itself for more chaining capabilities.
-			.syncCommands();
-
 		this.discord.run().then((client: ShardClient) => {
 			console.log(`'${client.user.name}' Discord Bot has logged in`);
 
@@ -43,7 +29,22 @@ export class DiscordBot extends Service {
 				},
 				status: "online",
 			};
+
 			client.gateway.setPresence(status);
+
+			const socket = this.discord.client.gateway.socket;
+			const creator = new SlashCreator({
+				applicationID: config.applicationId,
+				token: config.token,
+			});
+
+			creator.withServer(new GatewayServer((handler) => socket.on("INTERACTION_CREATE", handler)));
+			creator
+				// Registers all of your commands in the ./commands/ directory
+				.registerCommandsIn(path.join(__dirname, "commands"))
+				// This will sync commands to Discord, it must be called after commands are loaded.
+				// This also returns itself for more chaining capabilities.
+				.syncCommands();
 		});
 
 		this.discord.client.on("messageCreate", ev => {
