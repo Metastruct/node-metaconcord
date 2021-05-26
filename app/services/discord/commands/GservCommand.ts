39ae6daaae46a8bf51b4562e0045cbef7b4986f0
Techbot121 <techbot121@gmail.com>
2021-05-26 23:07:59 +0200

    change gserv command

--- app/services/discord/commands/GservCommand.ts
+++ app/services/discord/commands/GservCommand.ts
@@ -1,4 +1,5 @@
 import {
+	ApplicationCommandOption,
 	ApplicationCommandPermissionType,
 	CommandContext,
 	CommandOptionType,
@@ -10,14 +11,14 @@ import { NodeSSH } from "node-ssh";
 import EphemeralResponse from ".";
 import config from "ssh.json";
 
-const VALID_GSERV_PARAMS = {
-	rehash: true,
-	merge_repos: true,
-	kill: true,
-	rehashskeleton: true,
-	update_repos: true,
-	status: true,
-};
+const VALID_GSERV_PARAMS: [string, string][] = [
+	["rehash", "rehashes the server"],
+	["merge_repos", "prepares all repos for rehash"],
+	// ["kill", "Kills the server process (simulate crash)"],
+	["rehashskeleton", "commits skeleton changes (call rehash afterwards!)"],
+	["update_repos", "updates all svns/git repositories"],
+	["status", "show server status"],
+];
 
 export class SlashGservCommand extends SlashCommand {
 	constructor(bot: DiscordBot, creator: SlashCreator) {
@@ -35,57 +36,55 @@ export class SlashGservCommand extends SlashCommand {
 					},
 				],
 			},
-			options: [
-				{
-					type: CommandOptionType.STRING,
-					name: "param",
-					description: "gserv param",
-					required: false,
-				},
-			],
+			options: [],
 		});
 		this.filePath = __filename;
+
+		for (const param of VALID_GSERV_PARAMS) {
+			this.options.push({
+				name: param[0],
+				description: param[1],
+				type: CommandOptionType.SUB_COMMAND,
+			} as ApplicationCommandOption);
+		}
 	}
 
 	private async gserv(
 		ctx: CommandContext,
 		host: string,
+		username: string,
 		port: string,
 		param?: string
 	): Promise<boolean> {
-		if (!param) param = "rehash";
-		if (!VALID_GSERV_PARAMS[param]) return false;
+		const ssh = new NodeSSH();
+		await ssh.connect({
+			username: username,
+			host: host,
+			port: port,
+			privateKey: config.keyPath,
+		});
 
-		try {
-			const ssh = new NodeSSH();
-			await ssh.connect({
-				host: host,
-				port: port,
-				privateKey: config.keyPath,
-			});
+		let output = "";
+		await ssh.exec("gserv", [param], {
+			stream: "stderr",
+			onStdout: buff => (output += buff),
+			onStderr: buff => (output += buff),
+		});
 
-			let output = "";
-			await ssh.exec("gserv", [param], {
-				onStdout: buff => (output += buff),
-				onStderr: buff => (output += buff),
-			});
-
-			if (output.length > 1994) {
-				output = output.substring(0, 1990) + "...";
-			}
-
-			output = `\`\`\`${output}\`\`\``;
-			await ctx.send(output);
-		} catch (err) {
-			ctx.send(err);
-			return false;
+		if (output.length + host.length > 1994) {
+			output = output.substring(0, 1990 - host.length) + "...";
 		}
+
+		output = `${host}\n\`\`\`${output}\`\`\``;
+		await ctx.send(output);
+
+		return true;
 	}
 
 	async run(ctx: CommandContext): Promise<any> {
-		const param = ctx.options.param?.toString();
+		const param = Object.keys(ctx.options)[0];
 		const promises = config.servers.map(srvConfig =>
-			this.gserv(ctx, srvConfig.host, srvConfig.port, param)
+			this.gserv(ctx, srvConfig.host, srvConfig.username, srvConfig.port, param)
 		);
 
 		const results = await Promise.all(promises);
