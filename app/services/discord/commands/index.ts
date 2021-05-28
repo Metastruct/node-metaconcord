b0a9470b53ad02fb83e0cefa55ee0d383d702128
Techbot121 <techbot121@gmail.com>
2021-05-29 00:00:08 +0200

    use proper type for the EphemeralResponse function

--- app/services/discord/commands/index.ts
+++ app/services/discord/commands/index.ts
@@ -1,8 +1,5 @@
-import { InteractionResponseFlags } from "slash-create";
+import { MessageOptions } from "slash-create";
 
-export default function EphemeralResponse(content: string): {
-	content: string;
-	flags: InteractionResponseFlags;
-} {
-	return { content, flags: InteractionResponseFlags.EPHEMERAL };
+export default function EphemeralResponse(content: string): MessageOptions {
+	return { content, ephemeral: true };
 }
