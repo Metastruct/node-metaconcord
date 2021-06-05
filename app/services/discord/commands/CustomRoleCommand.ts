be4696b2c88f6bb90b009765d32a5fdd55118ecb
Techbot121 <techbot121@gmail.com>
2021-06-05 17:16:44 +0200

    update role command so you can add and remove roles

--- app/services/discord/commands/CustomRoleCommand.ts
+++ app/services/discord/commands/CustomRoleCommand.ts
@@ -15,25 +15,37 @@ export class SlashCustomRoleCommand extends SlashCommand {
 			guildIDs: [bot.config.guildId],
 			options: [
 				{
-					type: CommandOptionType.STRING,
-					name: "name",
-					description: "The name of your role",
-					required: true,
+					type: CommandOptionType.SUB_COMMAND,
+					name: "add",
+					description: "Adds a custom role",
+					options: [
+						{
+							type: CommandOptionType.STRING,
+							name: "name",
+							description: "The name of your role",
+							required: true,
+						},
+						{
+							type: CommandOptionType.INTEGER,
+							name: "red",
+							description: "The red component of your color",
+						},
+						{
+							type: CommandOptionType.INTEGER,
+							name: "green",
+							description: "The green component of your color",
+						},
+						{
+							type: CommandOptionType.INTEGER,
+							name: "blue",
+							description: "The blue component of your color",
+						},
+					],
 				},
 				{
-					type: CommandOptionType.INTEGER,
-					name: "red",
-					description: "The red component of your color",
-				},
-				{
-					type: CommandOptionType.INTEGER,
-					name: "green",
-					description: "The green component of your color",
-				},
-				{
-					type: CommandOptionType.INTEGER,
-					name: "blue",
-					description: "The blue component of your color",
+					type: CommandOptionType.SUB_COMMAND,
+					name: "remove",
+					description: "Removes your custom role",
 				},
 			],
 		});
@@ -42,10 +54,36 @@ export class SlashCustomRoleCommand extends SlashCommand {
 	}
 
 	async run(ctx: CommandContext): Promise<any> {
-		const roleName = ctx.options.name + ROLE_IDENTIFIER;
-		const r = ctx.options?.red?.toString() ?? "255",
-			g = ctx.options?.green?.toString() ?? "255",
-			b = ctx.options?.blue?.toString() ?? "255";
+		const cmd = Object.keys(ctx.options)[0];
+		switch (cmd) {
+			case "add":
+				return this.addRole(ctx);
+			case "remove":
+				return this.removeRole(ctx);
+			default:
+				return false; // ??? I guess
+		}
+	}
+
+	async removeRole(ctx: CommandContext): Promise<any> {
+		const guild = await this.bot.discord.guilds.resolve(ctx.guildID)?.fetch();
+		const member = await guild.members.fetch(ctx.member.id);
+		const hasRole = member.roles.cache.find(r => r.name.endsWith(ROLE_IDENTIFIER));
+		if (hasRole) {
+			await member.roles.remove(hasRole, "Removed role via command");
+			if (hasRole.members.size === 0) {
+				await hasRole.delete();
+			}
+		}
+		return hasRole
+			? EphemeralResponse("Removed your custom role")
+			: EphemeralResponse("You don't have a custom role...");
+	}
+	async addRole(ctx: CommandContext): Promise<any> {
+		const roleName = ctx.options.add.name + ROLE_IDENTIFIER;
+		const r = ctx.options.add?.red?.toString() ?? "255",
+			g = ctx.options.add?.green?.toString() ?? "255",
+			b = ctx.options.add?.blue?.toString() ?? "255";
 		const roleColor: Discord.ColorResolvable = [parseInt(r), parseInt(g), parseInt(b)];
 
 		const guild = await this.bot.discord.guilds.resolve(ctx.guildID)?.fetch();
@@ -67,14 +105,14 @@ export class SlashCustomRoleCommand extends SlashCommand {
 			}
 
 			targetRole = await roles.create({
-				reason: "Role command",
+				reason: "Added role via command",
 				data: {
 					name: roleName.toString(),
 					color: roleColor,
 				},
 			});
 		} else {
-			await targetRole.setColor(roleColor, "Role command");
+			await targetRole.setColor(roleColor, "Updated role via command");
 		}
 
 		await member.roles.add(targetRole);
