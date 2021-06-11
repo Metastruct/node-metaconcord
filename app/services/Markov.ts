0045730c317b1337715bb188fd688eeaa6fda073
Meta Construct <metastruct+github@metastruct.net>
2021-06-11 20:59:17 +0200

    migrate markov to db

--- app/services/Markov.ts
+++ app/services/Markov.ts
@@ -5,6 +5,7 @@ import Markov from "markov-strings";
 import fs from "fs";
 
 const MARKOV_DATA_PATH = "markov_data.txt";
+const MIGRATION_RATE = 50;
 
 export class MarkovService extends Service {
 	name = "Markov";
@@ -14,7 +15,7 @@ export class MarkovService extends Service {
 	constructor(container: Container) {
 		super(container);
 
-		(async () => {
+		setTimeout(async () => {
 			const sql = this.container.getService("Sql");
 			const db = await sql.getDatabase();
 			const hasTable = await sql.tableExists("markov");
@@ -23,26 +24,37 @@ export class MarkovService extends Service {
 			}
 
 			// legacy data
-			if (fs.existsSync(MARKOV_DATA_PATH)) {
-				fs.readFile(MARKOV_DATA_PATH, "utf8", (err, data) => {
+			/*if (fs.existsSync(MARKOV_DATA_PATH)) {
+				fs.readFile(MARKOV_DATA_PATH, "utf8", async (err, data) => {
 					if (err) {
 						console.log(err);
 						return;
 					}
 
 					const lines = data.split(EOL);
+					let promises = [];
+					let current = 0;
 					for (const line of lines) {
-						db.run("INSERT INTO markov (Data) VALUES (?)", line);
+						promises.push(db.run("INSERT INTO markov (Data) VALUES (?)", line));
+
+						if (promises.length % MIGRATION_RATE === 0) {
+							await Promise.all(promises);
+							promises = [];
+							current += MIGRATION_RATE;
+							console.log(`Processed ${current} / ${lines.length}`);
+						}
 					}
 
 					this.generator.addData(lines);
-					fs.unlinkSync(MARKOV_DATA_PATH);
+					//fs.unlinkSync(MARKOV_DATA_PATH);
 				});
-			}
+			}*/
 
-			const lines: Array<string> = await db.all("SELECT Data FROM markov;");
-			this.generator.addData(lines);
-		})();
+			const res = await db.all("SELECT * FROM markov;");
+			if (res.length > 0) {
+				this.generator.addData(res.map(row => row.Data));
+			}
+		}, 5000); // call after everything has initialized ?
 	}
 
 	private sanitizeString(input: string): string {
@@ -63,7 +75,7 @@ export class MarkovService extends Service {
 
 		const sql = this.container.getService("Sql");
 		const db = await sql.getDatabase();
-		db.run("INSERT INTO markov (Data) VALUES(?)", line);
+		await db.run("INSERT INTO markov (Data) VALUES(?)", line);
 	}
 
 	public generate(): string {
