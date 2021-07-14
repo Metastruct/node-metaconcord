cb7f72be6bd33f0550c3784b323bc76029af7800
Meta Construct <metastruct+github@metastruct.net>
2021-07-14 20:39:24 +0200

    note to self: dont try to be fancy

--- app/services/webapp/api/gamemode.ts
+++ app/services/webapp/api/gamemode.ts
@@ -1,22 +1,23 @@
 import { NodeSSH } from "node-ssh";
-import { Stream } from "stream";
 import { WebApp } from "..";
 import config from "@/ssh.json";
 import servers from "@/gamebridge.servers.json";
 
 const HOSTING_IDS = { 3: true };
 export default (webApp: WebApp): void => {
-	webApp.app.get("/gamemode/:id", async (req, res) => {
-		if (!servers.find(srv => srv.ip == req.ip.toString())) {
-			return res.status(503);
+	webApp.app.get("/gamemode/:id/", async (req, res) => {
+		const isOkIp =
+			servers.find(srv => srv.ip == req.ip.toString()) || req.ip.toString() === "127.0.0.1";
+		if (!isOkIp) {
+			return res.sendStatus(403);
 		}
 
 		const id = parseInt(req.params.id);
 		if (isNaN(id) || !HOSTING_IDS[id]) {
-			return res.status(503);
+			return res.sendStatus(403);
 		}
 
-		const srvConfig = config.servers[id];
+		const srvConfig = config.servers[id - 1];
 		const ssh = new NodeSSH();
 		await ssh.connect({
 			username: srvConfig.username,
@@ -25,13 +26,13 @@ export default (webApp: WebApp): void => {
 			privateKey: config.keyPath,
 		});
 
-		const output = new Stream.Writable();
-		ssh.exec("gserv", ["merge_repos", "rehash"], {
+		let output = "";
+		await ssh.exec("gserv", ["merge_repos", "rehash"], {
 			stream: "stderr",
-			onStdout: buff => output.write(buff),
-			onStderr: buff => output.write(buff),
-		}).finally(() => output.destroy());
+			onStdout: buff => (output += buff),
+			onStderr: buff => (output += buff),
+		});
 
-		return res.status(206).pipe(output);
+		return res.status(200).contentType("text/plain").send(output);
 	});
 };
