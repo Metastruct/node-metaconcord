869b7d83fbc28b68352e12d7f8b6fba999232eb0
Meta Construct <metastruct+github@metastruct.net>
2021-07-14 19:52:24 +0200

    should work

---
+++ app/services/webapp/api/gamemode.ts
@@ -0,0 +1,37 @@
+import { NodeSSH } from "node-ssh";
+import { Stream } from "stream";
+import { WebApp } from "..";
+import config from "@/ssh.json";
+import servers from "@/gamebridge.servers.json";
+
+const HOSTING_IDS = { 3: true };
+export default (webApp: WebApp): void => {
+	webApp.app.get("/gamemode/:id", async (req, res) => {
+		if (!servers.find(srv => srv.ip == req.ip.toString())) {
+			return res.status(503);
+		}
+
+		const id = parseInt(req.params.id);
+		if (isNaN(id) || !HOSTING_IDS[id]) {
+			return res.status(503);
+		}
+
+		const srvConfig = config.servers[id];
+		const ssh = new NodeSSH();
+		await ssh.connect({
+			username: srvConfig.username,
+			host: srvConfig.host,
+			port: srvConfig.port,
+			privateKey: config.keyPath,
+		});
+
+		const output = new Stream.Writable();
+		await ssh.exec("gserv", ["merge_repos", "rehash"], {
+			stream: "stderr",
+			onStdout: buff => output.write(buff),
+			onStderr: buff => output.write(buff),
+		});
+
+		return res.status(206).pipe(output);
+	});
+};
--- app/services/webapp/api/index.ts
+++ app/services/webapp/api/index.ts
@@ -1,5 +1,6 @@
 import addEmojiAPI from "./emojis";
 import addMapThumbnails from "./map-thumbnails";
 import addServerStatusAPI from "./server-status";
+import changeGamemode from "./gamemode";
 
-export default [addEmojiAPI, addServerStatusAPI, addMapThumbnails];
+export default [addEmojiAPI, addServerStatusAPI, addMapThumbnails, changeGamemode];
