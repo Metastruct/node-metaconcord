486293cf3bf7ac646217d7610e090b8ff981f58c
Techbot121 <techbot121@gmail.com>
2021-07-17 15:40:16 +0200

    add menu support to gserv

--- app/services/discord/commands/GservCommand.ts
+++ app/services/discord/commands/GservCommand.ts
@@ -1,33 +1,35 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import {
 	ApplicationCommandOption,
 	ApplicationCommandPermissionType,
+	ButtonStyle,
 	CommandContext,
-	CommandOptionType,
+	ComponentContext,
+	ComponentSelectOption,
+	ComponentType,
 	Message,
 	SlashCommand,
 	SlashCreator,
 } from "slash-create";
 import { DiscordBot } from "..";
 import { NodeSSH } from "node-ssh";
-import { TextChannel } from "discord.js";
+import { Emoji, TextChannel } from "discord.js";
 import config from "@/ssh.json";
 
-const VALID_GSERV_PARAMS: [string, string][] = [
+// order matters for the menu
+const VALID_GSERV_COMMANDS: [string, string][] = [
 	["rehash", "rehashes the server"],
 	["merge_repos", "prepares all repos for rehash"],
-	// ["kill", "Kills the server process (simulate crash)"],
 	["rehashskeleton", "commits skeleton changes (call rehash afterwards!)"],
 	["update_repos", "updates all svns/git repositories"],
 	["status", "show server status"],
 	["qu", "quick updates the repos"],
-	["qu_rehash", "quick rehashes the server"],
 ];
-
-const COMMAND_MAPPING: Map<string, Array<string>> = new Map<string, Array<string>>();
-COMMAND_MAPPING.set("qu_rehash", ["qu", "rehash"]);
 
 export class SlashGservCommand extends SlashCommand {
 	private bot: DiscordBot;
+	private commandOptions: ComponentSelectOption[] = [];
+	private serverOptions: ComponentSelectOption[] = [];
 	constructor(bot: DiscordBot, creator: SlashCreator) {
 		super(creator, {
 			name: "gserv",
@@ -43,47 +45,33 @@ export class SlashGservCommand extends SlashCommand {
 					},
 				],
 			},
-			options: [],
 		});
 		this.filePath = __filename;
 		this.bot = bot;
 
-		for (const param of VALID_GSERV_PARAMS) {
-			this.options.push({
-				name: param[0],
-				description: param[1],
-				type: CommandOptionType.SUB_COMMAND,
-				options: [
-					{
-						name: "server",
-						description: "the server to run the command on",
-						type: CommandOptionType.INTEGER,
-						choices: [
-							{
-								name: "g1",
-								value: 1,
-							},
-							{
-								name: "g2",
-								value: 2,
-							},
-							{
-								name: "g3",
-								value: 3,
-							},
-						],
-					},
-				],
-			} as ApplicationCommandOption);
+		for (const cmd of VALID_GSERV_COMMANDS) {
+			this.commandOptions.push({
+				label: cmd[0],
+				value: cmd[0],
+				description: cmd[1],
+			} as ComponentSelectOption);
+		}
+		for (const server of config.servers) {
+			this.serverOptions.push({
+				label: server.host.substr(0, 2), // [g1].metastruct.net
+				value: server.host.substr(1, 1), // g[1].metastruct.net
+				description: server.host, // g1.metastruct.net
+			} as ComponentSelectOption);
 		}
 	}
 
 	private async gserv(
-		ctx: CommandContext,
+		ctx: ComponentContext,
 		host: string,
 		username: string,
 		port: string,
-		param?: string
+		commands: string[],
+		solo: boolean
 	): Promise<boolean> {
 		const ssh = new NodeSSH();
 		await ssh.connect({
@@ -93,13 +81,8 @@ export class SlashGservCommand extends SlashCommand {
 			privateKey: config.keyPath,
 		});
 
-		let args = [param];
-		if (COMMAND_MAPPING.has(param)) {
-			args = COMMAND_MAPPING.get(param);
-		}
-
 		let output = "";
-		await ssh.exec("gserv", args, {
+		await ssh.exec("gserv", commands, {
 			stream: "stderr",
 			onStdout: buff => (output += buff),
 			onStderr: buff => (output += buff),
@@ -107,23 +90,23 @@ export class SlashGservCommand extends SlashCommand {
 
 		const success = !output.includes("GSERV FAILED");
 
-		const fileName = `${args.join("_")}_${host}_${Date.now()}.txt`;
+		const fileName = `${commands.join("_")}_${host}_${Date.now()}.txt`;
 		let msgContent = host;
 		if (!success) msgContent += " FAILED";
 
-		const response = await ctx.send({
+		const response = {
 			content: msgContent,
 			file: {
 				file: Buffer.from(output, "utf8"),
 				name: fileName,
 			},
-		});
+		};
 
-		if (response instanceof Message) {
-			const channel = (await this.bot.discord.channels.fetch(
-				response.channelID
-			)) as TextChannel;
-			const msg = await channel.messages.fetch(response.id);
+		const sent = solo ? await ctx.editParent(response) : await ctx.send(response);
+
+		if (sent instanceof Message) {
+			const channel = (await this.bot.discord.channels.fetch(sent.channelID)) as TextChannel;
+			const msg = await channel.messages.fetch(sent.id);
 			await msg.react(success ? "👍" : "👎");
 		}
 		return success;
@@ -131,18 +114,73 @@ export class SlashGservCommand extends SlashCommand {
 
 	async run(ctx: CommandContext): Promise<any> {
 		await ctx.defer();
-		const command = Object.keys(ctx.options)[0];
-		const server = (ctx.options[command] as any)?.server;
+		const user = ctx.user;
 
-		const promises = config.servers
-			.filter(
-				(srvConfig: { host: string }) =>
-					srvConfig.host.substr(1, 1) === server?.toString() || server == null
-			)
-			.map((srvConfig: { host: string; username: string; port: string }) =>
-				this.gserv(ctx, srvConfig.host, srvConfig.username, srvConfig.port, command)
-			);
+		let commands: string[];
+		await ctx.send("What command do you want to run?", {
+			components: [
+				{
+					type: ComponentType.ACTION_ROW,
+					components: [
+						{
+							type: ComponentType.SELECT,
+							custom_id: "gserv_command",
+							placeholder: "Choose a command.",
+							min_values: 1,
+							max_values: VALID_GSERV_COMMANDS.length,
+							options: this.commandOptions,
+						},
+					],
+				},
+			],
+		});
 
-		await Promise.all(promises);
+		ctx.registerComponent("gserv_command", async (selected: ComponentContext) => {
+			if (selected.user !== user) return;
+			commands = selected.values;
+
+			let servers: string[];
+			await selected.editParent("What server do you want the command to run on?", {
+				components: [
+					{
+						type: ComponentType.ACTION_ROW,
+						components: [
+							{
+								type: ComponentType.SELECT,
+								custom_id: "gserv_server",
+								placeholder: "Choose a server.",
+								min_values: 1,
+								max_values: config.servers.length,
+								options: this.serverOptions,
+							},
+						],
+					},
+				],
+			});
+			ctx.registerComponent("gserv_server", async (selected: ComponentContext) => {
+				if (selected.user !== user) return;
+				servers = selected.values;
+				selected.editParent(
+					`Running ${commands.join(" and ")} on ${servers.join(",")} please wait...`,
+					{ components: [] }
+				);
+				const promises = config.servers
+					.filter(
+						(srvConfig: { host: string }) =>
+							servers.find(srv => srvConfig.host.substr(1, 1) === srv) != undefined
+					)
+					.map((srvConfig: { host: string; username: string; port: string }) =>
+						this.gserv(
+							selected,
+							srvConfig.host,
+							srvConfig.username,
+							srvConfig.port,
+							commands,
+							servers.length === 1
+						)
+					);
+				await Promise.all(promises);
+			});
+		});
 	}
 }
