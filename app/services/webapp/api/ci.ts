4398d7fd19ddf97f72b67009cbbe0fe00b34cf89
Earu <ryan.rbsr@gmail.com>
2021-07-31 13:33:33 +0200

    CI

---
+++ app/services/webapp/api/ci.ts
@@ -0,0 +1,25 @@
+import { WebApp } from "..";
+import { spawn, spawnSync } from "child_process";
+import config from "@/ci.json";
+
+const FORBIDDEN = 403;
+const SUCCESS = 200;
+const ERROR = 500;
+
+export default (webApp: WebApp): void => {
+	webApp.app.get("/ci/reload", async (req, res) => {
+		const bearer = req.headers["authorization"]?.toLowerCase();
+		if (!bearer || bearer.length < 1) return res.status(FORBIDDEN).send();
+
+		const token = bearer.split("bearer ")[1];
+		if (token !== config.token) return res.status(FORBIDDEN).send();
+
+		try {
+			spawnSync("git pull", { cwd: __dirname });
+			spawn("sh deploy.sh", { cwd: __dirname });
+			return res.status(SUCCESS).send();
+		} catch (err) {
+			return res.status(ERROR).send(err.message);
+		}
+	});
+};
