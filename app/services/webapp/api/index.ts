182d24314a37eabe9672a3323fa812e9c257b11f
Earu <ryan.rbsr@gmail.com>
2021-07-31 13:49:23 +0200

    oopsies

--- app/services/webapp/api/index.ts
+++ app/services/webapp/api/index.ts
@@ -2,5 +2,6 @@ import addEmojiAPI from "./emojis";
 import addMapThumbnails from "./map-thumbnails";
 import addServerStatusAPI from "./server-status";
 import changeGamemode from "./gamemode";
+import ci from "./ci";
 
-export default [addEmojiAPI, addServerStatusAPI, addMapThumbnails, changeGamemode];
+export default [addEmojiAPI, addServerStatusAPI, addMapThumbnails, changeGamemode, ci];
