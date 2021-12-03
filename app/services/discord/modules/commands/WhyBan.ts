bc6bf7bbac32cf0369c5f888705c7f9c13efa1f6
Techbot121 <techbot121@gmail.com>
2021-12-03 20:53:00 +0100

    add steamid autocomplete to whyban

--- app/services/discord/modules/commands/WhyBan.ts
+++ app/services/discord/modules/commands/WhyBan.ts
@@ -1,4 +1,11 @@
-import { CommandContext, CommandOptionType, SlashCommand, SlashCreator } from "slash-create";
+import {
+	AutocompleteChoice,
+	AutocompleteContext,
+	CommandContext,
+	CommandOptionType,
+	SlashCommand,
+	SlashCreator,
+} from "slash-create";
 import { DiscordBot } from "../..";
 import { EphemeralResponse } from ".";
 import axios from "axios";
@@ -14,7 +21,8 @@ class MetaBan {
 }
 
 export class SlashWhyBanCommand extends SlashCommand {
-	private bot: DiscordBot;
+	private banCache: MetaBan[] = [];
+	private lastUpdate = 0;
 
 	constructor(bot: DiscordBot, creator: SlashCreator) {
 		super(creator, {
@@ -28,32 +36,56 @@ export class SlashWhyBanCommand extends SlashCommand {
 					name: "steamid",
 					description: "Your steamid in this format STEAM_0:0:000000000",
 					required: true,
+					autocomplete: true,
 				},
 			],
 		});
 		this.filePath = __filename;
-		this.bot = bot;
+	}
+
+	async updateCache(): Promise<void> {
+		const res = await axios.get<Array<MetaBan>>("http://g2.metastruct.net/bans");
+		if (res.status === 200) {
+			this.banCache = res.data;
+		}
+		this.lastUpdate = Date.now();
+	}
+
+	async getBan(steamid: string): Promise<MetaBan> {
+		if (Date.now() - this.lastUpdate > 5 * 60 * 1000) await this.updateCache();
+		const cached = this.banCache.find(ban => ban.sid === steamid);
+		if (cached) return cached;
+	}
+
+	async autocomplete(ctx: AutocompleteContext): Promise<AutocompleteChoice[]> {
+		return this.banCache
+			.filter(
+				function (ban) {
+					if (this.limit < 25) {
+						this.limit++;
+						return ban.sid.includes(ctx.options[ctx.focused]);
+					}
+				},
+				{ limit: 0 }
+			)
+			.map(ban => {
+				return { name: ban.sid, value: ban.sid };
+			});
 	}
 
 	async run(ctx: CommandContext): Promise<any> {
 		await ctx.defer(true);
-		const res = await axios.get<Array<MetaBan>>("http://g2.metastruct.net/bans");
-
-		if (res.status === 200) {
-			const ban = res.data.find(ban => ban.sid === ctx.options.steamid.toString());
-			if (!ban) return EphemeralResponse("That SteamID has never been banned before.");
-			if (!ban.b)
-				return EphemeralResponse(
-					`\`${ban.name}\` is currently not banned but was banned ${
-						ban.numbans !== undefined && ban.numbans > 1 ? `${ban.numbans} times ` : ""
-					}before.\nLast ban reason: \`${ban.banreason}\``
-				);
-
+		const ban = await this.getBan(ctx.options.steamid);
+		if (!ban) return EphemeralResponse("That SteamID has never been banned before.");
+		if (!ban.b)
 			return EphemeralResponse(
-				`User \`${ban.name}\` is currently banned for: \`${ban.banreason}\`\n expires in: <t:${ban.whenunban}:R>\n\`${ban.name}\` was banned \`${ban.numbans} times\` so far`
+				`\`${ban.name}\` is currently not banned but was banned ${
+					ban.numbans !== undefined && ban.numbans > 1 ? `${ban.numbans} times ` : ""
+				}before.\nLast ban reason: \`${ban.banreason}\``
 			);
-		} else {
-			return EphemeralResponse("Could not fetch ban data");
-		}
+
+		return EphemeralResponse(
+			`User \`${ban.name}\` is currently banned for: \`${ban.banreason}\`\n expires in: <t:${ban.whenunban}:R>\n\`${ban.name}\` was banned \`${ban.numbans} times\` so far`
+		);
 	}
 }
