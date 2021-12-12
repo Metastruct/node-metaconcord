ab9d2ff0f24c52e9766a3931c439d11cbbf3fa95
Techbot121 <techbot121@gmail.com>
2021-12-12 16:38:49 +0100

    don't try to get shit whilst the bot is down

--- app/services/gamebridge/discord/DiscordClient.ts
+++ app/services/gamebridge/discord/DiscordClient.ts
@@ -16,6 +16,7 @@ export default class DiscordClient extends Discord.Client {
 		const steam = gameServer.bridge.container.getService("Steam");
 
 		this.on("messageCreate", async ctx => {
+			if (!this.isReady()) return;
 			if (ctx.channel.id != this.gameServer.bridge.config.relayChannelId) return;
 			if (ctx.author.bot || !ctx.author.client) return;
 
