0d81c7c9578ad26e0e7c4f3355592d5fdfaa88fb
Meta Construct <metastruct+github@metastruct.net>
2022-02-10 15:00:01 +0100

    Don't kill the server connection if we somehow receive bad data

--- extensions/websocket.ts
+++ extensions/websocket.ts
@@ -1,7 +1,12 @@
 import { connection } from "websocket";
 
-declare module "websocket" {
-	interface connection {
-		sendPayload(name: string, payload): void;
+const drop = connection.prototype.drop;
+
+connection.prototype.drop = function (reasonCode, ...args) {
+	if (reasonCode == connection.CLOSE_REASON_INVALID_DATA) {
+		// console.warn("Would have dropped connection due to invalid UTF-8 data");
+		return;
 	}
-}
+
+	drop.apply(this, [reasonCode, ...args]);
+};
