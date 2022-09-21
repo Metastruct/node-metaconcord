fab92b63f58871cf619ed07d9e70d661fd7759e7
Meta Construct <metastruct+github@metastruct.net>
2022-09-21 14:14:49 +0200

    this is fixed for now so we can get back to this notation

--- app/services/gamebridge/payloads/ChatPayload.ts
+++ app/services/gamebridge/payloads/ChatPayload.ts
@@ -124,7 +124,7 @@ export default class ChatPayload extends Payload {
 		}
 
 		// 9312 = ①, 9313 = ②, and so on until 20
-		const serverId = `[${server.config.id}]`; // String.fromCodePoint(9311 + +(server.config.id ?? 0));
+		const serverId = `#${server.config.id}`; // String.fromCodePoint(9311 + +(server.config.id ?? 0));
 		await webhook
 			.send({
 				content: content,
