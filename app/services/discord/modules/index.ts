dff4f9a7fedda58a7054c55e7c0bb60654130adc
Techbot121 <techbot121@gmail.com>
2022-10-02 13:16:15 +0200

    surely nothing will go wrong with this

--- app/services/discord/modules/index.ts
+++ app/services/discord/modules/index.ts
@@ -3,6 +3,7 @@ import DiscordGuildIconLoader from "./discord-guild-icon";
 import LoggingLoader from "./logging";
 import MotdRollbackLoader from "./motdrollback";
 import RandomMsgLoader from "./randomMsg";
+import ShitpostLoader from "./shitposting";
 import StarboardLoader from "./starboard";
 
 export default [
@@ -10,6 +11,7 @@ export default [
 	LoggingLoader,
 	MotdRollbackLoader,
 	RandomMsgLoader,
+	ShitpostLoader,
 	StarboardLoader,
 	DiscordGuildIconLoader,
 ];
---
+++ app/services/discord/modules/shitposting.ts
@@ -0,0 +1,55 @@
+import { DiscordBot } from "..";
+import axios from "axios";
+import motdConfig from "@/config/motd.json";
+
+export default (bot: DiscordBot): void => {
+	bot.discord.on("messageCreate", async msg => {
+		if (msg.partial) {
+			try {
+				msg = await msg.fetch();
+			} catch {
+				return;
+			}
+		}
+		const id = bot.discord.user?.id;
+		if (!id) return;
+		if (
+			!(msg.mentions.repliedUser && msg.mentions.repliedUser.id === id) ||
+			!msg.mentions.users.has(id)
+		)
+			return;
+		if (
+			!bot.config.allowedShitpostingChannels.includes(msg.channelId) ||
+			msg.author.bot ||
+			msg.content.length === 0
+		)
+			return;
+		const rng = Math.random();
+
+		let reply = "";
+
+		if (rng > 0.4) {
+			const mk = await bot.container.getService("Markov")?.generate();
+			if (mk) reply = mk;
+		} else {
+			// todo: make an image cache from Motd.ts when it fetches the images instead.
+			const res = await axios.get(
+				`https://api.imgur.com/3/album/${motdConfig.imgurAlbumId}/images`,
+				{
+					headers: {
+						Authorization: `Client-ID ${motdConfig.imgurClientId}`,
+					},
+				}
+			);
+			if (res.status === 200) {
+				const urls: Array<any> = res.data.data;
+				const image = urls[Math.floor(Math.random() * urls.length)];
+				reply = image.link;
+			}
+		}
+
+		if (reply !== "") {
+			await msg.reply(reply);
+		}
+	});
+};
