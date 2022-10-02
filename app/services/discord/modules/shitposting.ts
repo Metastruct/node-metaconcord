f8148f9ef44699411d4d7bf37cad024ff7c8ecec
Techbot121 <techbot121@gmail.com>
2022-10-02 13:41:10 +0200

    lol!

--- app/services/discord/modules/shitposting.ts
+++ app/services/discord/modules/shitposting.ts
@@ -13,7 +13,7 @@ export default (bot: DiscordBot): void => {
 		}
 		const id = bot.discord.user?.id;
 		if (!id) return;
-		if (!(msg.mentions.repliedUser?.id === id) || !(msg.mentions.users.first()?.id === id))
+		if (!(msg.mentions.repliedUser?.id === id) && !(msg.mentions.users.first()?.id === id))
 			return;
 		if (!bot.config.allowedShitpostingChannels.includes(msg.channelId) || msg.author.bot)
 			return;
