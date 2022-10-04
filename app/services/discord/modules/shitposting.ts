0453b4452871dc66b26d81992a7dac58bd8cfb67
Techbot121 <techbot121@gmail.com>
2022-10-04 19:02:40 +0200

    even less images

--- app/services/discord/modules/shitposting.ts
+++ app/services/discord/modules/shitposting.ts
@@ -22,7 +22,7 @@ export default (bot: DiscordBot): void => {
 
 		let reply = "";
 
-		if (rng > 0.2) {
+		if (rng > 0.1) {
 			const mk = await bot.container.getService("Markov")?.generate();
 			if (mk) reply = mk;
 		} else {
