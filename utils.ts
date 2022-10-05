d315d50e3245dc50ef8e5affb96838e4e9e231fa
Techbot121 <techbot121@gmail.com>
2022-10-05 20:34:01 +0200

    why is this not a thing yet

--- utils.ts
+++ utils.ts
@@ -1,4 +1,6 @@
 export const sleep = (ms: number): Promise<any> => new Promise(resolve => setTimeout(resolve, ms));
+export const clamp = (input: number, min: number, max: number): number =>
+	input <= min ? min : input >= max ? max : input;
 export const f = (
 	name: string,
 	value: string,
