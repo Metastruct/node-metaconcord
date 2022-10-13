0788c19c5c9e3bdfd30a6ee7c6fc14550ddc8207
Techbot121 <techbot121@gmail.com>
2022-10-13 16:31:38 +0200

    remove wip let's do this as a payload instead

--- app/services/webapp/api/index.ts
+++ app/services/webapp/api/index.ts
@@ -3,13 +3,5 @@ import addGameServerStatusAPI from "./game-server-status";
 import addMapThumbnails from "./map-thumbnails";
 import changeGamemode from "./gamemode";
 import ci from "./ci";
-import errorHandler from "./errorhandler";
 
-export default [
-	addEmojiAPI,
-	addGameServerStatusAPI,
-	addMapThumbnails,
-	changeGamemode,
-	ci,
-	errorHandler,
-];
+export default [addEmojiAPI, addGameServerStatusAPI, addMapThumbnails, changeGamemode, ci];
