7d945007848cbac48a614951aab088035f4a33f4
Techbot121 <techbot121@gmail.com>
2022-11-10 20:49:00 +0100

    add cursed way of getting part of file from a repo

--- app/services/webapp/api/gmod-error-handler.ts
+++ app/services/webapp/api/gmod-error-handler.ts
@@ -1,10 +1,12 @@
 import { APIEmbed } from "discord.js";
 import { GameBridge, GameServer, Player } from "../../gamebridge";
 import { WebApp } from "..";
+import { clamp } from "@/utils";
 import Discord from "discord.js";
 import SteamID from "steamid";
 import config from "@/config/webapp.json";
 import express from "express";
+import request, { gql } from "graphql-request";
 import servers from "@/config/gamebridge.servers.json";
 
 type GmodResponse = {
@@ -20,10 +22,27 @@ type GmodResponse = {
 	v: string;
 };
 
+type StackMatchGroups = {
+	addon?: string;
+	cmdname?: string;
+	cmdrealm?: string;
+	engine?: string;
+	ext?: string;
+	filename?: string;
+	fn: string;
+	lino: string;
+	nick?: string;
+	path?: string;
+	rfilename?: string;
+	stacknr: string;
+	steamid: string;
+};
+
 const AddonURIS = {
 	acf: "https://github.com/metastruct/ACF/blob/master/",
 	advdupe2: "https://github.com/wiremod/advdupe2/blob/master/",
 	aowl: "https://gitlab.com/metastruct/internal/aowl/-/blob/master/",
+	easychat: "https://github.com/Earu/EasyChat/blob/master/",
 	epoe: "https://github.com/Metastruct/EPOE/blob/master/",
 	fast_addons: "https://gitlab.com/metastruct/internal/fast_addons/-/blob/master/",
 	gcompute: "https://github.com/Metastruct/gcompute/blob/master/",
@@ -67,6 +86,59 @@ const SuperReplacer = (_: string, ...args: any[]) => {
 
 const gamemodes = ["sandbox_modded", "mta", "jazztronauts"]; //proper gamemode support when???
 
+async function getFrame(
+	provider: string,
+	smg: StackMatchGroups,
+	url: string
+): Promise<string | undefined> {
+	const isGithub = provider === "github";
+	const endpoint = isGithub ? "https://api.github.com/graphql" : "https://gitlab.com/api/graphql";
+	const repo = smg.addon;
+	const owner = url.match(/\.com\/(.+?)\//);
+	const branch = url.split("/").at(-2);
+
+	if (!owner) return;
+	const query = isGithub
+		? gql`{
+		repository(owner:${owner[1]}, name:${repo}) {
+			content: object(expression: "${branch}:${smg.path}") {
+				... on Blob {
+					text
+				}
+			}
+		}
+}
+`
+		: gql`{
+	project(fullpath:${url.match(/\.com\/(.+?)\/\-/)}) {
+		repository {
+			blobs(paths: ${smg.path}){
+				nodes{rawTextBlob}
+			}
+		}
+	}
+}
+`;
+	try {
+		const res = await request(endpoint, query);
+		if (res) {
+			const filecontent = isGithub
+				? (res.data.repository.content.text as string)
+				: (res.data.project.repository.blobs.nodes[0].rawTextBlob as string);
+			const line = Number(smg.lino);
+			const lines = filecontent.split(/\r?\n/);
+			return lines
+				.slice(clamp(line - 3, 0, lines.length), clamp(line + 3, 0, lines.length))
+				.join();
+		}
+		console.error(res);
+		return;
+	} catch (err) {
+		console.error(JSON.stringify(err, undefined, 2));
+		return;
+	}
+}
+
 export default (webApp: WebApp): void => {
 	let gameBridge: GameBridge;
 	const webhook = new Discord.WebhookClient({
@@ -85,10 +157,11 @@ export default (webApp: WebApp): void => {
 		gameBridge = gameBridge || webApp.container.getService("GameBridge");
 
 		const server = servers.find(srv => srv.ip === ip);
-		let gameserver: GameServer | undefined;
+		let gameserver: GameServer;
 		let player: Player | undefined;
 		if (server) {
-			gameserver = gameBridge.servers.find(server => server.config.ip === ip);
+			// ip matched so it HAS to exist
+			gameserver = gameBridge.servers.filter(server => server.config.ip === ip)[0];
 		} else {
 			const allplayers = gameBridge.servers.reduce(
 				(ps, cs) => ps.concat(cs.status.players),
@@ -106,6 +179,7 @@ export default (webApp: WebApp): void => {
 		if (body.stack) {
 			if (body.error.startsWith("@repl_")) return;
 			const stack = body.stack.replaceAll(megaRex, SuperReplacer);
+			const embeds: APIEmbed[] = [];
 			const embed: APIEmbed = {
 				description: stack.replace("`", "\\`"),
 				footer: {
@@ -125,18 +199,56 @@ export default (webApp: WebApp): void => {
 					icon_url: (player.avatar as string) ?? undefined,
 					url: `https://steamcommunity.com/profiles/[U:1:${player.accountId}]`,
 				};
+				if (player.isLinux) embed.fields = [{ name: "OS:", value: "UNIX", inline: true }];
 			}
 			if (server) {
 				embed.author = {
 					name: server.name,
 				};
+				if (gameserver) {
+					embed.fields = [
+						{
+							name: "Map Uptime:",
+							value: `<t:${gameserver?.mapUptime.toString()}:R>`,
+							inline: true,
+						},
+					];
+					if (
+						gameserver.config.defaultGamemode &&
+						body.gamemode !== gameserver.config.defaultGamemode
+					) {
+						embed.fields.push({
+							name: "Gamemode:",
+							value: `${gameserver.gamemode.name} (${gameserver.gamemode.folderName})`,
+							inline: true,
+						});
+					}
+				}
+			}
+			embeds.push(embed);
+			const matches = [...body.stack.matchAll(megaRex)];
+			const filematch = matches.filter(
+				m => !m.groups?.engine && Number(m.groups?.stacknr) < 3
+			);
+			if (filematch.length > 0) {
+				const smg = filematch[0].groups as StackMatchGroups;
+				const url: string | undefined = smg.addon ? AddonURIS[smg.addon] : undefined;
+				if (url) {
+					const provider = url.match(/([^\.\/]+)\.com/);
+					if (!provider) return;
+					await getFrame(provider[1], smg, url).then(res => {
+						if (res) {
+							embeds.push({ description: `\`\`\`lua\n${res}\`\`\`` });
+						}
+					});
+				}
 			}
 			if (body.v === "test") return;
 			webhook
 				.send({
 					allowedMentions: { parse: [] },
 					content: `**${body.error.replace("*", "\\*")}**`,
-					embeds: [embed],
+					embeds: embeds,
 				})
 				.catch(console.error);
 		}
