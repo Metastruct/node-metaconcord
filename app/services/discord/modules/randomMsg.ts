7af0067152b72bded1ab6631489f5aba40efb80d
Techbot121 <techbot121@gmail.com>
2022-11-19 13:53:00 +0100

    forgot context

--- app/services/discord/modules/randomMsg.ts
+++ app/services/discord/modules/randomMsg.ts
@@ -46,7 +46,12 @@ export default (bot: DiscordBot): void => {
 		if (its_posting_time && !posting) {
 			await sendShat(msg.content);
 			replied = false;
-		} else if (!its_posting_time && !replied && !posting) {
+		} else if (
+			!its_posting_time &&
+			!replied &&
+			!posting &&
+			msg.mentions.users.first()?.id === bot.discord.user?.id
+		) {
 			await sendShat(msg.content);
 			replied = true;
 		}
