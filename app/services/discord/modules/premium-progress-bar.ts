98c15835d5ebc02d46a7335996f254798fb273b2
Techbot121 <techbot121@gmail.com>
2022-11-19 16:04:55 +0100

    show progressbar depending on how close we are

--- app/services/discord/modules/premium-progress-bar.ts
+++ app/services/discord/modules/premium-progress-bar.ts
@@ -1,11 +1,19 @@
 import { DiscordBot } from "..";
 
-const DESIRED_LEVEL = 3; // 2 is also ok but eh just for testing
+const LVL_MAP = [
+	[0, 2],
+	[1, 7],
+	[2, 14],
+	[3, 14],
+];
 
 export default (bot: DiscordBot): void => {
 	const setProgressBar = async () => {
 		const guild = await bot.discord.guilds.fetch(bot.config.guildId);
-		if (guild.premiumTier < DESIRED_LEVEL) {
+		const count = guild.premiumSubscriptionCount;
+		if (!count) return;
+		const needed = LVL_MAP[guild.premiumTier][1];
+		if (count / needed >= 0.5) {
 			if (!guild.premiumProgressBarEnabled) {
 				await guild.setPremiumProgressBarEnabled(true);
 			}
