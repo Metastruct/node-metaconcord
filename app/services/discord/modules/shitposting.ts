30412795c491809b368c4cd6f1f6f5bdf6e6f0ae
Techbot121 <techbot121@gmail.com>
2022-12-24 11:31:35 +0100

    wrong index

--- app/services/discord/modules/shitposting.ts
+++ app/services/discord/modules/shitposting.ts
@@ -202,7 +202,7 @@ export default (bot: DiscordBot): void => {
 				TRIGGER_WORDS.some(str => msg.content.toLowerCase().includes(str)) ||
 				(lastMsgs.length > 1 &&
 					lastMsgs.slice(-2)[0].author.id === id &&
-					lastMsgs.slice(-4)[0].author.id !== id)) &&
+					lastMsgs.slice(-3)[0].author.id !== id)) &&
 			bot.config.chatChannelId === msg.channelId
 		) {
 			const its_posting_time = Date.now() - lastMkTime > MSG_REPLY_INTERVAL;
