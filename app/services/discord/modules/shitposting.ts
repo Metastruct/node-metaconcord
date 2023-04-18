babf1526654c4706530f7a0a813a5aab033f6ca0
Techbot121 <techbot121@gmail.com>
2023-04-18 15:23:45 +0200

    allow people to mention the bot again in 10 minutes

--- app/services/discord/modules/shitposting.ts
+++ app/services/discord/modules/shitposting.ts
@@ -7,6 +7,7 @@ import EmojiList from "unicode-emoji-json/data-ordered-emoji.json";
 // #chat and #shat constants
 const ACTIVITY_CHANGE_INTERVAL = 1000 * 60 * 60 * 0.25; // interval for changing the bot status to a random message
 const MSG_INTERVAL = 1000 * 60; // interval for checking messages for below trigger count, also resets activity if it was set manually or by other means
+const MSG_REPLY_INTERVAL = 1000 * 60 * 10; // interval when to allow replying to messages
 const MSG_TRIGGER_COUNT = 13; // how many msgs in above interval until a msg is posted
 const MSG_CHAT_INTERVAL = 1000 * 60 * 60 * 2; // total time until a message is forced if below interval wasn't met (active chatters)
 const MSG_DEAD_CHAT_REVIVAL_INTERVAL = 1000 * 60 * 60 * 0.75; // idle (no active chatters) time until post, can be delayed by chatting
@@ -214,6 +215,9 @@ export default (bot: DiscordBot): void => {
 
 		setInterval(async () => {
 			const now = Date.now();
+			if (now - lastMsgTime >= MSG_REPLY_INTERVAL) {
+				replied = false;
+			}
 			if (
 				lastMsgs.length > 0 &&
 				lastMsgs.slice(-1)[0].author.id !== bot.discord.user?.id &&
