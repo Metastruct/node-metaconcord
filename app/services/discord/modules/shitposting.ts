4acd8f0eeabdd7b081b83baffdc57f3c5268ba06
Techbot121 <techbot121@gmail.com>
2023-05-23 20:42:52 +0200

    uncomment this for now since imgur doesn't want to cooperate

--- app/services/discord/modules/shitposting.ts
+++ app/services/discord/modules/shitposting.ts
@@ -75,31 +75,27 @@ export const Shat = async (
 		return mk ? { content: mk.replace(`<@${DiscordConfig.bot.userId}> `, "") } : undefined;
 	} else {
 		const images = globalThis.MetaConcord.container.getService("Motd")?.images;
-		const word = msg && !msg.startsWith("http") ? getWord(msg) : undefined;
+		let word = msg && !msg.startsWith("http") ? getWord(msg) : undefined;
 
 		if (!word && !images)
+			word = getWord(await globalThis.MetaConcord.container.getService("Markov")?.generate());
+
+		// if (Math.random() <= 0.5 || !word) {
+		// 	const imgur = images[(Math.random() * images.length) | 0];
+		// 	const result = await makeSpeechBubble(imgur.link, Math.random() <= 0.5);
+		// 	return result
+		// 		? { files: [{ attachment: result, description: imgur.title }] }
+		// 		: undefined;
+		// } else {
+		const res = await globalThis.MetaConcord.container.getService("Tenor")?.search(word, 4);
+		if (!res)
 			return {
 				content: await globalThis.MetaConcord.container.getService("Markov")?.generate(),
-			};
-
-		if (Math.random() <= 0.5 || !word) {
-			const imgur = images[(Math.random() * images.length) | 0];
-			const result = await makeSpeechBubble(imgur.link, Math.random() <= 0.5);
-			return result
-				? { files: [{ attachment: result, description: imgur.title }] }
-				: undefined;
-		} else {
-			const res = await globalThis.MetaConcord.container.getService("Tenor")?.search(word, 4);
-			if (!res)
-				return {
-					content: await globalThis.MetaConcord.container
-						.getService("Markov")
-						?.generate(),
-				}; // if for some reason we get no result;
-			return {
-				content: res.data.results[(Math.random() * res.data.results.length) | 0].url,
-			};
-		}
+			}; // if for some reason we get no result;
+		return {
+			content: res.data.results[(Math.random() * res.data.results.length) | 0].url,
+		};
+		// }
 	}
 };
 
