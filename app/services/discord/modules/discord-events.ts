c7005563661dcd37da1cfb1452655d2ca1472ab5
Techbot121 <techbot121@gmail.com>
2023-07-21 18:14:24 +0200

    add ability to create automated events via code only for now (?)

--- app/services/discord/modules/discord-events.ts
+++ app/services/discord/modules/discord-events.ts
@@ -2,10 +2,21 @@ import { DiscordBot } from "..";
 import { join } from "path";
 import Discord from "discord.js";
 import DiscordConfig from "@/config/discord.json";
+import dayjs from "dayjs";
 
 const events = [
 	{
 		icon: "vr",
+		eventData: {
+			entityType: Discord.GuildScheduledEventEntityType.Voice,
+			privacyLevel: Discord.GuildScheduledEventPrivacyLevel.GuildOnly,
+			channel: DiscordConfig.channels.gamingVoice,
+			scheduledStartTime: dayjs().day(6).hour(20).minute(0).toDate(),
+			name: "VRChat [Automated Event]",
+			description:
+				"Wanna join us in VR? Sign up so we know. Discussion in <#1009704968070107148>\n\nAlso checkout our Group at https://vrchat.com/home/group/grp_caeaacf4-a8a7-4e7d-8b66-46094732f85b",
+			reason: "Automated Event",
+		} as Discord.GuildScheduledEventCreateOptions,
 		triggers: ["vrchat", "vr"],
 	},
 ];
@@ -14,10 +25,37 @@ const iconsPath = join(process.cwd(), "resources/discord-event-icons");
 export default (bot: DiscordBot): void => {
 	const data = bot.container.getService("Data");
 	if (!data) return;
+
 	const GetParticipants = async (event: Discord.GuildScheduledEvent) => {
 		const eventUsers = await event.fetchSubscribers({ withMember: true });
 		return eventUsers.map(evu => evu.member);
 	};
+
+	const checkEvents = async () => {
+		const guild = bot.getGuild();
+		if (!guild) return;
+		const eventList = await guild.scheduledEvents.fetch();
+		for (const { eventData } of events) {
+			const existingEvent = eventList.find(event => event.name === eventData.name);
+			if (existingEvent?.status === Discord.GuildScheduledEventStatus.Active) return;
+			if (existingEvent) {
+				const eventDate = existingEvent.scheduledStartAt;
+				if (eventDate && dayjs().add(5, "minutes").toDate() > eventDate) {
+					await guild.scheduledEvents.delete(existingEvent);
+					await guild.scheduledEvents.create({
+						...eventData,
+						scheduledStartTime: dayjs().day(6).hour(20).minute(0).toDate(),
+					});
+				}
+			} else {
+				await guild.scheduledEvents.create(eventData);
+			}
+		}
+	};
+
+	bot.discord.on("ready", async () => {
+		await checkEvents();
+	});
 
 	bot.discord.on("guildScheduledEventUpdate", async (_, now) => {
 		const event = now;
@@ -55,5 +93,6 @@ export default (bot: DiscordBot): void => {
 				break;
 			}
 		}
+		await checkEvents();
 	});
 };
