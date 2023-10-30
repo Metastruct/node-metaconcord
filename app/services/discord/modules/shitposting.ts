b2cf73920eadfd80e80e0a6aed66e25571b631a8
Techbot121 <techbot121@gmail.com>
2023-10-30 18:02:51 +0100

    adjust emoji and image freq

--- app/services/discord/modules/shitposting.ts
+++ app/services/discord/modules/shitposting.ts
@@ -29,10 +29,10 @@ const MAYBE_TRIGGER_FREQ = 0.4; // frequency of triggers above
 
 // shat constants
 const TENOR_IMAGE_FREQ = 0.1; // how often the bot will respond with an image instead of text
-const DISCORD_IMAGE_FREQ = 0.2;
+const DISCORD_IMAGE_FREQ = 0.15;
 const STICKER_FREQ = 0.05; // guess
 const REPLY_FREQ = 0.5; // when to take a word from a previous discord message if provided
-const EMOJI_REPLY_FREQ = 0.1; // reply with just an emoji
+const EMOJI_REPLY_FREQ = 0.2; // reply with just an emoji
 
 const ALLOWED_IMG_PROVIDERS = ["tenor", "imgur", "discordapp", "tumblr"];
 
