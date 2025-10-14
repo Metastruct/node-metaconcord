d9e65a8d8914be327a4c93e8a600aec0a500b590
Meta Construct <metastruct+github@metastruct.net>
2025-10-15 00:29:02 +0200

    display map thumbnail as bg again

--- app/services/webapp/api/game-server-status.ts
+++ app/services/webapp/api/game-server-status.ts
@@ -3,6 +3,19 @@ import GameServer from "@/app/services/gamebridge/GameServer.js";
 import nodeHtmlToImage from "node-html-to-image";
 import path from "path";
 import pug from "pug";
+import { access, mkdir, readFile, writeFile } from "fs/promises";
+import { constants as fs_constants } from "fs";
+import mime from "mime";
+
+const cacheFolder = path.join(process.cwd(), "cache", "map-thumbnails");
+
+const imageToDataURL = async filePath => {
+	const mimeType = mime.getType(filePath);
+	if (!mimeType) throw new Error("No MIME type from path?");
+
+	const base64 = await readFile(filePath, { encoding: "base64" });
+	return `data:${mimeType};base64,${base64}`;
+};
 
 export default async (webApp: WebApp): Promise<void> => {
 	webApp.app.get("/server-status/:id{/:bruh}", async (req, res) => {
@@ -25,11 +38,56 @@ export default async (webApp: WebApp): Promise<void> => {
 			!req.headers.accept ||
 			req.headers["user-agent"]?.includes("+https://discordapp.com");
 
+		// #region Map Thumbnail
+		const mapThumbnail = server.status.mapThumbnail;
+		const workshopMap = server.workshopMap;
+		let thumbFilepath: string | undefined = undefined;
+		if (mapThumbnail?.match(/^https?:\/\//) && workshopMap) {
+			await access(cacheFolder, fs_constants.F_OK).catch(() =>
+				mkdir(cacheFolder, { recursive: true })
+			);
+
+			const _thumbFilepath = path.join(cacheFolder, `${workshopMap.id}`);
+			let hasFile = true;
+			// have to assume these are the only extensions we expect...
+			for (const ext of [".webp", ".jpg", ".jpeg", ".png", ".gif"]) {
+				await access(_thumbFilepath + ext, fs_constants.F_OK).catch(() => {
+					hasFile = false;
+				});
+				if (hasFile) {
+					thumbFilepath = _thumbFilepath + ext;
+					break;
+				}
+			}
+			if (!thumbFilepath) {
+				try {
+					const response = await fetch(mapThumbnail);
+					if (response.ok) {
+						const contentType = response.headers.get("content-type");
+						if (!contentType) throw new Error("No content-type");
+
+						const ext = mime.getExtension(contentType);
+						if (!ext) throw new Error("No extension");
+
+						thumbFilepath = [_thumbFilepath, ext.toLowerCase()].join(".");
+
+						const buffer = Buffer.from(await response.arrayBuffer());
+						await writeFile(thumbFilepath, buffer);
+					}
+				} catch (err) {}
+			}
+		} else {
+			thumbFilepath = mapThumbnail as string;
+		}
+		const mapThumbnail64 = await imageToDataURL(thumbFilepath);
+		// #endregion
+
 		const html = pug.renderFile(
 			path.join(process.cwd(), "resources/game-server-status/view.pug"),
 			{
 				server,
 				image: !!discordBot,
+				mapThumbnail64,
 			}
 		);
 		if (discordBot) {
