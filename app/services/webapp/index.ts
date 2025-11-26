eed2f1f08373cdcaca26bab4266cb8131a02918d
techbot <techbot121@gmail.com>
2025-11-26 18:08:47 +0000

    don't show the base here either

--- app/services/webapp/index.ts
+++ app/services/webapp/index.ts
@@ -17,7 +17,7 @@ export class WebApp extends Service {
 	constructor(container: Container) {
 		super(container);
 
-		this.app.use(pinoHttp());
+		this.app.use(pinoHttp({ base: undefined }));
 
 		for (const addAPI of APIs) {
 			addAPI(this);
