c34c8912f2a09441e6eb2f06c86b3af940b69a5a
techbot <techbot121@gmail.com>
2025-12-29 02:34:09 +0100

    minor format change [skip ci]

--- app/services/webapp/index.ts
+++ app/services/webapp/index.ts
@@ -8,7 +8,7 @@ import { logger } from "@/utils.js";
 
 const log = logger("WebApp");
 
-const PATH_IGNORE = ["/server-status","/discord/guild/emojis", "/gmod/errors"];
+const PATH_IGNORE = ["/server-status", "/discord/guild/emojis", "/gmod/errors"];
 
 export class WebApp extends Service {
 	name = "WebApp";
@@ -24,7 +24,7 @@ export class WebApp extends Service {
 				logger: log,
 				base: undefined,
 				level: process.env.LOG_LEVEL || "info",
-				autoLogging: { ignore: req => PATH_IGNORE.some(p => req.path.startsWith(p))},
+				autoLogging: { ignore: req => PATH_IGNORE.some(p => req.path.startsWith(p)) },
 			})
 		);
 
