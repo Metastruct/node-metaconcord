c6d1949ce9f66a27828f43c43bd8962f652f27a7
techbot <techbot121@gmail.com>
2026-09-17 23:08:32 +0200

    the old uri works again
    This reverts commit 315c0353371dbc8b396e4c1d94186fe4facea2cc.

--- app/services/gamebridge/games/gmod/handlers/StatusPayload.ts
+++ app/services/gamebridge/games/gmod/handlers/StatusPayload.ts
@@ -282,7 +282,7 @@ export default class StatusPayload extends Payload {
 						.setStyle(Discord.ButtonStyle.Link)
 						.setLabel("Connect")
 						.setURL(
-							`https://metaconcord.metastruct.net/${
+							`https://metastruct.net/${
 								server.config.label ? "join/" + server.config.label : ""
 							}`
 						)
